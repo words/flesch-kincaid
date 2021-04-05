da2fea5886531a610e56f89854267bcfed3ed185
Titus Wormer <tituswormer@gmail.com>
2021-04-05 15:30:28 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ var sentenceWeight = 0.39
 var wordWeight = 11.8
 var adjustment = 15.59
 
+/**
+ * @typedef {Object.<string, number>} FleschKincaidCounts
+ * @property {number} sentence
+ * @property {number} word
+ * @property {number} syllable
+ */
+
+/**
+ * Given an object containing the number of words (`word`), the number of sentences (`sentence`), and the number of syllables  (`syllable`) in a document, returns the U.S. grade level associated with the document.
+ * Values can theoretically start at `-3.40` and end at `Infinity`.
+ *
+ * @param {FleschKincaidCounts} counts
+ * @returns {number}
+ */
 export function fleschKincaid(counts) {
   if (!counts || !counts.sentence || !counts.word || !counts.syllable) {
     return Number.NaN
--- test.js
+++ test.js
@@ -2,11 +2,16 @@ import test from 'tape'
 import {fleschKincaid} from './index.js'
 
 test('fleschKincaid', function (t) {
+  // @ts-ignore runtime
   t.ok(Number.isNaN(fleschKincaid()), 'NaN when an invalid value is given')
   t.equal(round(fleschKincaid({sentence: 1, word: 13, syllable: 26})), 13.08)
   t.end()
 })
 
+/**
+ * @param {number} value
+ * @returns {number}
+ */
 function round(value) {
   return Math.round(value * 1e6) / 1e6
 }
