6f5e039c6b14df88d44a4811980821e6177f935a
Titus Wormer <tituswormer@gmail.com>
2022-11-01 13:53:11 +0100

    Add docs to types

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
+/**
+ * @typedef Counts
+ *   Counts from input document.
+ * @property {number} sentence
+ *   Number of sentences.
+ * @property {number} word
+ *   Number of words.
+ * @property {number} syllable
+ *   Number of syllables.
+ */
+
+/**
+ * @typedef {Counts} FleschKincaidCounts
+ *   Deprecated: please use the `Counts` type instead.
+ */
+
 const sentenceWeight = 0.39
 const wordWeight = 11.8
 const adjustment = 15.59
 
 /**
- * @typedef {Object.<string, number>} FleschKincaidCounts
- * @property {number} sentence
- * @property {number} word
- * @property {number} syllable
- */
-
-/**
- * Given an object containing the number of words (`word`), the number of sentences (`sentence`), and the number of syllables  (`syllable`) in a document, returns the U.S. grade level associated with the document.
+ * Given an object containing the number of words (`word`), the number of
+ * sentences (`sentence`), and the number of syllables  (`syllable`) in a
+ * document, returns the U.S. grade level associated with the document.
  * Values can theoretically start at `-3.40` and end at `Infinity`.
  *
- * @param {FleschKincaidCounts} counts
+ * @param {Counts} counts
+ *   Counts from input document.
  * @returns {number}
+ *   Grade level associated with the document.
  */
 export function fleschKincaid(counts) {
   if (!counts || !counts.sentence || !counts.word || !counts.syllable) {
